a89af115028a7b3f6d0a8cfce875f93b779f5855
Paweł Burgchardt <pawel.burgchardt@gmail.com>
2021-07-09 16:36:02 +0200

    move viewmodel class to separate file, created slider with cpu count in settings page

---
+++ Model/SingleGpuDescriptor.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GolemUI
+{
+    /// <summary>
+    /// Interaction logic for DashboardSettings.xaml
+    /// </summary>
+    /// 
+
+    public class SingleGpuDescriptor : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyChange([CallerMemberName] string? propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public string _Name { get; set; }
+        public bool _IsActive { get; set; }
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                _Name = value;
+                NotifyChange("Name");
+            }
+        }
+        public bool IsActive
+        {
+            get { return _IsActive; }
+            set
+            {
+                _IsActive = value;
+                NotifyChange("Name");
+            }
+        }
+        public SingleGpuDescriptor(string name, bool isActive)
+        {
+            Name = name;
+            IsActive = isActive;
+        }
+    }
+}
--- UI/DashboardSettings.xaml.cs
+++ UI/DashboardSettings.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using GolemUI.Settings;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,68 +15,19 @@ using System.Windows.Shapes;
 
 namespace GolemUI
 {
-    /// <summary>
-    /// Interaction logic for DashboardSettings.xaml
-    /// </summary>
-    /// 
-
-    public class SingleGpuDescriptor
-    {
-        public string Name { get; set; }
-        public bool IsActive { get; set; }
-
-        public SingleGpuDescriptor(string name, bool isActive)
-        {
-            Name = name;
-            IsActive = isActive;
-        }
-    }
-    public class SettingsDataContext
-    {
-        public List<SingleGpuDescriptor> GpuList { get; set; }
-        public SettingsDataContext()
-        {
-            GpuList = new List<SingleGpuDescriptor>();
-            GpuList.Add(new SingleGpuDescriptor("1st GPU", false));
-            GpuList.Add(new SingleGpuDescriptor("second GPU", true));
-            GpuList.Add(new SingleGpuDescriptor("3rd GPU", false));
-        }
-    }
     public partial class DashboardSettings : UserControl
     {
-        public string[] GpuList { get; set; } = { "raz", "dwat" };
-        SettingsDataContext ctx = new SettingsDataContext();
+
+        SettingsViewModel ctx = new SettingsViewModel();
         public DashboardSettings()
         {
             InitializeComponent();
             ctx.GpuList.Add(new SingleGpuDescriptor("4th GPU", true));
             
-            GlobalApplicationState.Instance.ApplicationStateChanged += OnGlobalApplicationStateChanged;
-            GpuList = new[] { "ABC", "DEF" };
+          
             this.DataContext = this.ctx;
         }
 
-
-
-        public void OnGlobalApplicationStateChanged(object sender, GlobalApplicationStateEventArgs? args)
-        {
-            if (args != null)
-            {
-                switch(args.action)
-                {
-                    case GlobalApplicationStateAction.reloadSettings:
-                        break;
-                    case GlobalApplicationStateAction.yagnaAppStarting:
-                        break;
-                    case GlobalApplicationStateAction.yagnaAppStopped:
-                        break;
-                }
-            }
-        }
-
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
+    
     }
 }
