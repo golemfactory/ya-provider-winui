0404264d6ede37d84bf342fa953e55a0385eaf23
Paweł Burgchardt <pawel.burgchardt@gmail.com>
2021-07-22 19:35:14 +0200

    progressing with dashboard refactor

---
+++ Converters/DashboardStatusToTextConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using GolemUI;
+namespace GolemUI.Converters
+{
+    [ValueConversion(typeof(DashboardStatusEnum?), typeof(string))]
+    public class DashboardStatusToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var baseValue = value as DashboardStatusEnum?;
+            if (baseValue == null)
+            {
+                return "";
+            }
+            return baseValue switch
+            {
+                DashboardStatusEnum.Error => "error",
+                DashboardStatusEnum.Hidden => "",
+                DashboardStatusEnum.Idle => "ready for tasks",
+                DashboardStatusEnum.Mining => "mining",
+                _ => ""
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Model/DashboardStatusEnum.cs
@@ -0,0 +1,11 @@
+﻿using GolemUI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolemUI
+{
+  public enum DashboardStatusEnum { Mining,Idle,Error, Hidden}
+}
