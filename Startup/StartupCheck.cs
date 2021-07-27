1cd8cf76c53a9e535b99307685a4a3a56a1715fe
scx1332 <sieciech.czajka@golem.network>
2021-07-27 15:34:07 +0200

    Startup check

---
+++ Startup/StartupCheck.cs
@@ -0,0 +1,16 @@
+using GolemUI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolemUI.Startup
+{
+    class StartupCheck
+    { 
+
+
+    }
+
+}
