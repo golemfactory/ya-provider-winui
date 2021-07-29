3a656c5d7051ad585224035306788372c7ea999e
scx1332 <sieciech.czajka@golem.network>
2021-07-29 15:49:05 +0200

    Added kill process tree extension

--- Command/Claymore.cs
+++ Command/Claymore.cs
@@ -10,7 +10,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-
+using GolemUI.Extensions;
 namespace GolemUI.Command
 {
 
@@ -86,34 +86,7 @@ namespace GolemUI.Command
         }
 
 
-        /// <summary>
-        /// Kill a process, and all of its children, grandchildren, etc.
-        /// </summary>
-        /// <param name="pid">Process ID.</param>
-        private static void KillProcessAndChildren(int pid)
-        {
-            // Cannot close 'system idle process'.
-            if (pid == 0)
-            {
-                return;
-            }
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher
-                    ("Select * From Win32_Process Where ParentProcessID=" + pid);
-            ManagementObjectCollection moc = searcher.Get();
-            foreach (ManagementObject mo in moc)
-            {
-                KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
-            }
-            try
-            {
-                Process proc = Process.GetProcessById(pid);
-                proc.Kill();
-            }
-            catch (ArgumentException)
-            {
-                // Process already exited.
-            }
-        }
+
         public void Stop()
         {
 
@@ -121,7 +94,7 @@ namespace GolemUI.Command
             {
                 if (!_claymoreProcess.HasExited)
                 {
-                    KillProcessAndChildren(_claymoreProcess.Id);
+                    _claymoreProcess.Kill(true);
                 }
                 _claymoreProcess = null;
             }
---
+++ Extensions/KillProcess.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolemUI.Extensions
+{
+    // Extension methods must be defined in a static class.
+    public static class ProcessExtension
+    {
+        /// <summary>
+        /// Kill a process, and all of its children, grandchildren, etc.
+        /// </summary>
+        /// <param name="pid">Process ID.</param>
+        private static void KillProcessAndChildren(int pid)
+        {
+            // Cannot close 'system idle process'.
+            if (pid == 0)
+            {
+                return;
+            }
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher
+                    ("Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection moc = searcher.Get();
+            foreach (ManagementObject mo in moc)
+            {
+                KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+            }
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                proc.Kill();
+            }
+            catch (ArgumentException)
+            {
+                // Process already exited.
+            }
+        }
+
+
+        // This is the extension method.
+        // The first parameter takes the "this" modifier
+        // and specifies the type for which the method is defined.
+        public static void Kill(this Process process, bool entireProcessTree)
+        {
+            if (entireProcessTree)
+            {
+                try
+                {
+                    KillProcessAndChildren(process.Id);
+                }
+                catch (Exception ex)
+                {
+                    Debug.Fail(ex.Message);
+                }
+            }
+            else if (!entireProcessTree)
+            {
+                process.Kill();
+            }
+
+        }
+    }
+}
