98f702b7dabb8e8980ea0550ab2f31ed4792fa01
scx1332 <sieciech.czajka@golem.network>
2021-08-02 13:49:35 +0200

    Dashboard settings advanced stubs

---
+++ ViewModel/SettingsAdvViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GolemUI.Interfaces;
+using GolemUI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolemUI.ViewModel
+{
+    public class SettingsAdvViewModel
+    {
+        private readonly IUserSettingsProvider _userSettingsProvider;
+
+        public UserSettings _userSettings;
+
+        public SettingsAdvViewModel(IUserSettingsProvider userSettingsProvider)
+        {
+            _userSettingsProvider = userSettingsProvider;
+
+            _userSettings = _userSettingsProvider.LoadUserSettings();
+        }
+
+        public void SaveData()
+        {
+            _userSettingsProvider.SaveUserSettings(_userSettings);
+        }
+
+
+
+
+    }
+}
