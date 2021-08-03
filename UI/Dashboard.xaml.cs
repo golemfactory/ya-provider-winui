40b5a607303c3350d0bee3c17382019b25517924
Paweł Burgchardt <pawel.burgchardt@gmail.com>
2021-08-03 19:11:33 +0200

    notification baloon when app goes to tray

--- UI/Dashboard.xaml.cs
+++ UI/Dashboard.xaml.cs
@@ -128,6 +128,7 @@ namespace GolemUI
             else
             {
                 tbNotificationIcon.Visibility = Visibility.Visible;
+                tbNotificationIcon.ShowBalloonTip("Torg Miner is still running in tray", "To close application use Thorg's tray's context menu.", Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Info);
                 this.WindowState = WindowState.Minimized;
                 this.ShowInTaskbar = false;
             }
@@ -172,6 +173,7 @@ namespace GolemUI
             if (ls.MinimizeToTrayOnMinimize)
             {
                 tbNotificationIcon.Visibility = Visibility.Visible;
+                tbNotificationIcon.ShowBalloonTip("Torg Miner is still running in tray", "To close application use Thorg's tray's context menu.", Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Info);
                 this.WindowState = WindowState.Minimized;
                 this.ShowInTaskbar = false;
             }
