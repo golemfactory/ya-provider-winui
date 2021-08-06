7cfde87b0841bd25d6e3bedf1687309dbc4abb1f
Paweł Burgchardt <pawel.burgchardt@gmail.com>
2021-08-06 09:51:51 +0200

    draft notification service

---
+++ Interfaces/INotificationsService.cs
@@ -0,0 +1,24 @@
+﻿using GolemUI.ViewModel;
+
+namespace GolemUI
+{
+
+    public delegate void NewNotificationEventHandler(INotificationObject notification);
+    public interface INotificationsService
+    {
+        public event NewNotificationEventHandler? NotificationArrived;
+        public event NewNotificationEventHandler? NotificationUpdated;
+        public event NewNotificationEventHandler? NotificationDeleted;
+        public void PushNotification(INotificationObject notification);
+        public void UpdateNotification(INotificationObject notification);
+        public void DeleteNotification(INotificationObject notification);
+
+    }
+
+    public interface INotificationObject
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Src/NotificationsService.cs
@@ -0,0 +1,49 @@
+﻿using NBitcoin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GolemUI.Src
+{
+
+    public class SimpleNotificationObject: INotificationObject
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class NotificationsService : INotificationsService
+    {
+        public NotificationsService()
+        {
+        }
+
+        public event NewNotificationEventHandler? NotificationArrived;
+        public event NewNotificationEventHandler? NotificationUpdated;
+        public event NewNotificationEventHandler? NotificationDeleted;
+        public void PushNotification(INotificationObject notification)
+        {
+
+        }
+        public void UpdateNotification(INotificationObject notification)
+        {
+
+        }
+        public void DeleteNotification(INotificationObject notification)
+        {
+
+        }
+    }
+}
