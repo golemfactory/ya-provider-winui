0cd2792272bc0bd6fa673ef4eaa877e988488a16
scx1332 <sieciech.czajka@golem.network>
2021-08-10 17:15:12 +0200

    Status hidden at the start.

--- ViewModel/DashboardMainViewModel.cs
+++ ViewModel/DashboardMainViewModel.cs
@@ -253,7 +253,7 @@ namespace GolemUI.ViewModel
         public int _enabledGpuCount;
         public int _enabledCpuCount;
 
-        public DashboardStatusEnum _status = DashboardStatusEnum.Ready;
+        public DashboardStatusEnum _status = DashboardStatusEnum.Hidden;
         public DashboardStatusEnum Status
         {
             get => _status;
