3bb65f1a136e4464bbdb638602cb485607f6c3ca
scx1332 <sieciech.czajka@golem.network>
2021-08-11 18:22:41 +0200

    Removed unused code

--- Src/EstimatedEarningsProvider.cs
+++ Src/EstimatedEarningsProvider.cs
@@ -23,49 +23,10 @@ namespace GolemUI.Src
             _historyDataProvider = historyDataProvider;
         }
 
-
-        public double? ConvertGlmPerGhToDollarsPerDay(double? glmPerGh)
-        {
-            if (glmPerGh == null)
-            {
-                return null;
-            }
-            const double secondsInDay = 3600 * 24;
-            const double GhToMh = 0.001;
-            double valueInUsd = (double)_priceProvider.CoinValue((decimal)(glmPerGh.Value * secondsInDay * GhToMh), IPriceProvider.Coin.GLM);
-            return valueInUsd;
-        }
-
-        public double? ConvertEthPerGhToDollarsPerDay(double? dayEthPerGh)
-        {
-            if (dayEthPerGh == null)
-            {
-                return null;
-            }
-            double ethValueInUsd = (double)_priceProvider.CoinValue(1.0m, IPriceProvider.Coin.ETH);
-            double valueInUSD = dayEthPerGh.Value * ethValueInUsd;
-            return valueInUSD;
-        }
-
-        public double? ConvertEtcPerGhToDollarsPerDay(double? dayEtcPerGh)
-        {
-            if (dayEtcPerGh == null)
-            {
-                return null;
-            }
-            double ethValueInUsd = (double)_priceProvider.CoinValue(1.0m, IPriceProvider.Coin.ETC);
-            double valueInUSD = dayEtcPerGh.Value * ethValueInUsd;
-            return valueInUSD;
-        }
-
-
         double IEstimatedProfitProvider.HashRateToUSDPerDay(double hashRate, IEstimatedProfitProvider.Coin coin)
         {
             return EarningsCalculator.HashRateToUSDPerDay(hashRate, _historyDataProvider, coin, _priceProvider, _remoteSettingsProvider);
         }
-
-
-        //
     }
 
 }
