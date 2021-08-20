488e390c9345d18188066481dba6bb1a4796ea4c
Paweł Burgchardt <pawel.burgchardt@gmail.com>
2021-08-20 14:02:28 +0200

    wip

---
+++ UI/Dialogs/DlgUpdateApp.xaml.cs
@@ -0,0 +1,62 @@
+﻿
+using GolemUI.ViewModel;
+using GolemUI.ViewModel.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GolemUI.UI.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for DlgEditAddress.xaml
+    /// </summary>
+    public partial class DlgUpdateApp : Window
+    {
+
+        public DlgUpdateApp(DlgEditAddressViewModel model)
+        {
+            InitializeComponent();
+            DataContext = model;
+        }
+
+        public DlgUpdateAppViewModel? Model => DataContext as DlgUpdateAppViewModel;
+
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+           /* if (Model != null)
+                Model.ChangeAction = DlgEditAddressViewModel.Action.None;*/
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            if (Model != null)
+            {
+               /* Model.ChangeAction = Model.ShouldTransferFunds ? DlgEditAddressViewModel.Action.TransferOut : DlgEditAddressViewModel.Action.Change;*/
+            }
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+          /*  if (Model != null)
+                Model.ChangeAction = DlgEditAddressViewModel.Action.None;*/
+            this.DialogResult = false;
+            this.Close();
+        }
+
+    }
+}
---
+++ ViewModel/Dialogs/DlgUpdateAppViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GolemUI.Interfaces;
+using Nethereum.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GolemUI.ViewModel.Dialogs
+{
+    public class DlgUpdateAppViewModel
+    {
+        public string UpdateLink => "http://google.com";
+        public string CurrentVersion => System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        public String NewVersion => "2.0.0";
+        public String AppName => "Miner App";
+        public String AppCodeName => "[CandyFlip]";
+        public String ChangeLog => "-change 1 \nchange 2\n change 3\n change 4";
+        public bool ForceUpdate => true;
+    }
+}
