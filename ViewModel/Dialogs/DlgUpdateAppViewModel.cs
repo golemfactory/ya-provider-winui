081d96feef89d641dd70140e7a5703e5e0b2a147
scx1332 <sieciech.czajka@golem.network>
2021-08-20 18:04:26 +0200

    Cleanup code

--- ViewModel/Dialogs/DlgUpdateAppViewModel.cs
+++ ViewModel/Dialogs/DlgUpdateAppViewModel.cs
@@ -18,36 +18,21 @@ namespace GolemUI.ViewModel.Dialogs
         public string Title => IsUpToDate ? "Your application is up to date" : "New version of application is available";
         public string UpdateLink { private set; get; }
         public string CurrentVersion { private set; get; }
-        string NewVersion { set; get; }
-        private string AppCodeName { set; get; }
-        public string ChangeLog
-        {
-            get
-            {
-                if (_changes.Count == 0)
-                    return "";
-                return "New changes:\n" + String.Join("\n", _changes.Take(5).Select(x => "- " + x).ToArray());
-            }
-        }
+        private string _newVersion { set; get; }
+        private string _appCodeName { set; get; }
+        public string ChangeLog => _changes.Count == 0 ? "" : "New changes:\n" + String.Join("\n", _changes.Take(5).Select(x => "- " + x).ToArray());
         private List<string> _changes = new List<string>();
         public bool ShouldForceUpdate { private set; get; }
-        public bool IsUpToDate => VersionUtil.AreVersionsEqual(CurrentVersion, NewVersion);
-        public string NewVersionDisplayString
-        {
-            get
-            {
-                if (String.IsNullOrEmpty(AppCodeName))
-                    return NewVersion;
-                return $"{NewVersion} [{AppCodeName}]";
-            }
-        }
+        public bool IsUpToDate => VersionUtil.AreVersionsEqual(CurrentVersion, _newVersion);
+        public string NewVersionDisplayString => String.IsNullOrEmpty(_appCodeName) ? _newVersion : $"{_newVersion} [{_appCodeName}]";
+        
 
         public DlgUpdateAppViewModel(string updateLink, string currentVersion, string newVersion, string appCodeName, List<string> changes, bool shouldForceUpdate)
         {
             UpdateLink = updateLink;
             CurrentVersion = currentVersion;
-            NewVersion = newVersion;
-            AppCodeName = appCodeName;
+            _newVersion = newVersion;
+            _appCodeName = appCodeName;
             _changes = changes;
             ShouldForceUpdate = shouldForceUpdate;
         }
