a2bf5a55772bd198f71f225156af1d408b57d1ae
Paweł Burgchardt <pawel.burgchardt@gmail.com>
2021-08-23 18:09:32 +0200

    cleared comments

--- Src/Sentry/SentryUserFeedback.cs
+++ Src/Sentry/SentryUserFeedback.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using GolemUI.Interfaces;
+using GolemUI.Utils;
 using Sentry;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
@@ -13,25 +14,28 @@ namespace GolemUI.Src
 
     public class SentryUserFeedbackService : IUserFeedbackService
     {
-        /*  private readonly IProcessControler _processControler;
-          private readonly Interfaces.IProviderConfig _providerConfig;*/
+      
         private SentryContext Context = new SentryContext();
 
-
-
-        public SentryUserFeedbackService(/*Interfaces.IProcessControler processControler, Interfaces.IProviderConfig providerConfig*/)
+        public SentryUserFeedbackService()
         {
-            /* _processControler = processControler;
-             _providerConfig = providerConfig;
-             _providerConfig.PropertyChanged += ProviderConfig_PropertyChanged;
-             _processControler.PropertyChanged += _processControler_PropertyChanged;
-             Context.MemberChanged += Context_MemberChanged;*/
-
         }
+
         public void SendUserFeedback(string tag, string name, string email, string comments)
         {
+            SentrySdk.ConfigureScope(scope =>
+            {
+                scope.AddAttachment(PathUtil.GetLocalBenchmarkPath());
+                scope.AddAttachment(PathUtil.GetRemoteSettingsPath());
+                scope.AddAttachment(PathUtil.GetLocalSettingsPath());
+            });
             var eventID = Sentry.SentrySdk.CaptureMessage(tag);
             Sentry.SentrySdk.CaptureUserFeedback(new Sentry.UserFeedback(eventID, name, email, comments));
+            SentrySdk.ConfigureScope(scope =>
+            {
+                scope.ClearAttachments();
+            });
         }
+            
     }
 }
