b325dd7155e3a76af6e3259481ccd1bf60f83c57
scx1332 <sieciech.czajka@golem.network>
2021-09-13 19:43:38 +0200

    LazySubnet

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace GolemUI.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("LazyPolyNet")]
+        [global::System.Configuration.DefaultSettingValueAttribute("LazySubnet")]
         public string Subnet {
             get {
                 return ((string)(this["Subnet"]));
@@ -34,7 +34,7 @@ namespace GolemUI.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool TestNet {
             get {
                 return ((bool)(this["TestNet"]));
