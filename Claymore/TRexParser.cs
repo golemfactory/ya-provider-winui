b781179beae674bb96f643a53cec80f002e18bc1
scx1332 <sieciech.czajka@golem.network>
2021-10-27 15:20:53 +0200

    Working on trex support

---
+++ Claymore/TRexParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolemUI.Claymore
+{
+    class TRexParser : ICloneable, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BeforeParsing(bool enableRecording)
+        {
+        }
+
+        public void SetFinished()
+        {
+
+        }
+
+        public void ParseLine(string lineText)
+        {
+
+        }
+    }
+}
---
+++ GolemUISetup/ExternalBinaries/plugins/t-rex/FIRO-mintpond.bat
@@ -0,0 +1,2 @@
+t-rex.exe -a mtp -o stratum+ssl://firo.mintpond.com:3005 -u aBR3GY8eBKvEwjrVgNgSWZsteJPpFDqm6U.rig0 -p x --fork-at firopow=time:2021-10-26T06:00:00
+pause
